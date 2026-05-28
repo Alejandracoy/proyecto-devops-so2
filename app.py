957cca970b3f4e1259e05a75c2285d6b5c709905
Alejandra <86064847+Alejandracoy@users.noreply.github.com>
2026-05-28 11:42:25 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.get('/')
+def hello():
+    return {
+        "status": "success",
+        "message": "¡Mi infraestructura DevOps en AWS funciona!",
+        "team": "Proyecto Final SO2",
+        "container_id": os.uname()[1]
+    }
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
